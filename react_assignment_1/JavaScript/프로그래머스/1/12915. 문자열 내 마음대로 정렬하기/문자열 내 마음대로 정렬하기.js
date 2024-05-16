17a6804fc7abd291ac954e14aa69f47720d689ce
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-05-16 10:36:53 +0900

    쓸모없는거 다 삭제

--- react_assignment_1/JavaScript/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++
@@ -1,8 +0,0 @@
-function solution(strings, n) {
-  return strings.sort((a, b) => {
-    if (a[n] === b[n]) {
-      return a.localeCompare(b);
-    }
-    return a[n].localeCompare(b[n]);
-  });
-}
--- react_assignment_1/JavaScript/프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++
@@ -1,8 +0,0 @@
-function solution(arr) {
-    var answer = 0;
-    const addNum = arr.reduce((prev, cur) => {
-        return prev + cur;
-    }, 0);
-    answer = addNum/arr.length;
-    return answer;
-}
